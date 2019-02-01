a34ade7a8e52d6994c99c799c98cb314457d68ca
wernejm <wernejm@rose-hulman.edu>
2019-01-31 20:20:49 -0500

    Done.

--- src/problem5.py
+++ src/problem5.py
@@ -3,9 +3,9 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         James Werne.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,9 +145,24 @@ def problem5(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    lis = []
+
+    for k in range(len(seq_of_seq)):
+        inner_seq = seq_of_seq[k]
+        if len(inner_seq) > 0:
+            largest_value = inner_seq[0]
+            for j in range(len(inner_seq)):
+                if inner_seq[j] > largest_value:
+                    largest_value = inner_seq[j]
+
+            lis = lis + [largest_value]
+
+    return lis
+
 
 
 ###############################################################################
